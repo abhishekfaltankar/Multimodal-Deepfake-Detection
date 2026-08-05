a1950129e78921072f705302fbe19b084e036f3e
abhishek faltankar <abhishekfaltankar21@gmail.com>
2026-08-05 15:49:40 +0530

    Implement first training epoch with EfficientNet-B0

--- training/train.py
+++ training/train.py
@@ -1,12 +1,18 @@
 import torch
+import torch.nn as nn
 
 from torch.utils.data import DataLoader, random_split
 
 from training.dataset import DeepfakeDataset
+from models.efficientnet import DeepfakeDetector
 
+# Device
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+print(f"Using Device: {device}")
+
+# Dataset
 dataset = DeepfakeDataset("dataset/processed/faces")
-
-print(f"Total Images : {len(dataset)}")
 
 train_size = int(0.7 * len(dataset))
 val_size = int(0.15 * len(dataset))
@@ -35,6 +41,70 @@ test_loader = DataLoader(
     shuffle=False
 )
 
-print(f"Training Images : {len(train_dataset)}")
-print(f"Validation Images : {len(val_dataset)}")
-print(f"Testing Images : {len(test_dataset)}")
+
+# Model
+model = DeepfakeDetector().to(device)
+
+# Loss Function
+criterion = nn.CrossEntropyLoss()
+
+
+# Optimizer
+optimizer = torch.optim.Adam(
+    model.parameters(),
+    lr=0.0001
+)
+
+print("\nModel Loaded Successfully!")
+print(f"Loss Function : {criterion.__class__.__name__}")
+print(f"Optimizer     : {optimizer.__class__.__name__}")
+
+print(model)
+
+images, labels = next(iter(train_loader))
+
+images = images.to(device)
+labels = labels.to(device)
+
+outputs = model(images)
+
+print(f"\nBatch Shape : {images.shape}")
+print(f"Output Shape: {outputs.shape}")
+
+
+# Training (1 Epoch)
+model.train()
+
+running_loss = 0.0
+correct = 0
+total = 0
+
+for images, labels in train_loader:
+
+    images = images.to(device)
+    labels = labels.to(device)
+
+    optimizer.zero_grad()
+
+    outputs = model(images)
+
+    loss = criterion(outputs, labels)
+
+    loss.backward()
+
+    optimizer.step()
+
+    running_loss += loss.item()
+
+    _, predicted = torch.max(outputs, 1)
+
+    total += labels.size(0)
+
+    correct += (predicted == labels).sum().item()
+
+epoch_loss = running_loss / len(train_loader)
+epoch_accuracy = 100 * correct / total
+
+print("\nTraining Completed!")
+print(f"Loss     : {epoch_loss:.4f}")
+print(f"Accuracy : {epoch_accuracy:.2f}%")
